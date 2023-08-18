4acf5a1fb6c4ba66d36a32f14a32f538df8bde9a
Anele Funani <anele.funani@standardbank.co.za>
2023-08-18 09:14:35 +0200

    Bug Fix: Update SpringBoot version. closes #2

--- src/main/java/co/za/code/eagleeye/configserver/Application.java
+++ src/main/java/co/za/code/eagleeye/configserver/Application.java
@@ -1,9 +1,16 @@
 package co.za.code.eagleeye.configserver;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Application running...");
+        SpringApplication.run(Application.class);
     }
 }
